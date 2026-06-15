770a9b1ef6d47c08cd68f51e2db140658c804ed0
linegeek <10746488+linegeek@users.noreply.github.com>
2026-06-15 07:51:02 -0700

    feat: add recursive-descent parser (expr, term, factor, SUM)

--- src/engine/formulaParser.ts
+++ src/engine/formulaParser.ts
@@ -61,3 +61,105 @@ function tokenize(input: string): Token[] {
 
   return tokens;
 }
+
+class Parser {
+  private tokens: Token[];
+  private pos = 0;
+
+  constructor(tokens: Token[]) {
+    this.tokens = tokens;
+  }
+
+  private peek(): Token | undefined {
+    return this.tokens[this.pos];
+  }
+
+  private consume(type?: TokenType): Token {
+    const t = this.tokens[this.pos++];
+    if (!t) throw new Error('PARSE_ERROR');
+    if (type && t.type !== type) throw new Error('PARSE_ERROR');
+    return t;
+  }
+
+  parseExpr(): (cells: Record<CellId, number>) => number {
+    let left = this.parseTerm();
+    while (this.peek()?.type === 'PLUS' || this.peek()?.type === 'MINUS') {
+      const op = this.consume().type;
+      const right = this.parseTerm();
+      const prev = left;
+      left = op === 'PLUS'
+        ? (c) => prev(c) + right(c)
+        : (c) => prev(c) - right(c);
+    }
+    return left;
+  }
+
+  private parseTerm(): (cells: Record<CellId, number>) => number {
+    let left = this.parseFactor();
+    while (this.peek()?.type === 'STAR' || this.peek()?.type === 'SLASH') {
+      const op = this.consume().type;
+      const right = this.parseFactor();
+      const prev = left;
+      left = op === 'STAR'
+        ? (c) => prev(c) * right(c)
+        : (c) => {
+            const d = right(c);
+            if (d === 0) throw new Error('DIV_ZERO');
+            return prev(c) / d;
+          };
+    }
+    return left;
+  }
+
+  private parseFactor(): (cells: Record<CellId, number>) => number {
+    const t = this.peek();
+    if (!t) throw new Error('PARSE_ERROR');
+
+    if (t.type === 'NUMBER') {
+      this.consume();
+      const val = parseFloat(t.value);
+      return () => val;
+    }
+
+    if (t.type === 'CELL_REF') {
+      this.consume();
+      const id = t.value as CellId;
+      return (cells) => {
+        if (!(id in cells)) throw new Error('REF_ERROR');
+        return cells[id];
+      };
+    }
+
+    if (t.type === 'SUM') {
+      this.consume();
+      this.consume('LPAREN');
+      const start = this.consume('CELL_REF').value as CellId;
+      this.consume('COLON');
+      const end = this.consume('CELL_REF').value as CellId;
+      this.consume('RPAREN');
+      const ids = expandRange(start, end);
+      return (cells) =>
+        ids.reduce((acc, id) => {
+          if (!(id in cells)) throw new Error('REF_ERROR');
+          return acc + cells[id];
+        }, 0);
+    }
+
+    if (t.type === 'LPAREN') {
+      this.consume();
+      const inner = this.parseExpr();
+      this.consume('RPAREN');
+      return inner;
+    }
+
+    throw new Error('PARSE_ERROR');
+  }
+
+  get currentPos(): number {
+    return this.pos;
+  }
+
+  get totalTokens(): number {
+    return this.tokens.length;
+  }
+}
